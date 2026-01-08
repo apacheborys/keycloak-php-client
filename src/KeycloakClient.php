047ff900220f9c1f7a20a1c245bd76c752be2ea1
Borys Yermokhin <borys_ermokhin@yahoo.com>
2026-01-08 18:19:18 +0100

    add bundle skeleton

---
+++ src/KeycloakClient.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Apacheborys\KeycloakPhpClient;
+
+final class KeycloakClient
+{
+}
---
+++ src/Symfony/DependencyInjection/KeycloakPhpClientExtension.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Apacheborys\KeycloakPhpClient\Symfony\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+final class KeycloakPhpClientExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+    }
+}
